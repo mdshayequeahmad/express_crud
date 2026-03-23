98d7966c25e6d2126a46745d1b21366463a0f895
mdshayequeahmad <shayeque01@gmail.com>
2026-03-23 20:38:51 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const studentRoutes = require("./routes/students");
+const courseRoutes = require("./routes/courses");
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Student & Course Portal API!");
+});
+
+app.use("/students", studentRoutes);
+app.use("/courses", courseRoutes);
+
+app.use((req, res) => {
+  res.status(404).send("Page not found");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
