8fc31d5fb3439dd502c26a41572bb290db33b46b
mdshayequeahmad <shayeque01@gmail.com>
2026-03-24 15:37:53 +0530

    Controllers Task

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const userController = require('../controllers/userController');
 
-router.get("/", (req, res) => {
-  res.send("Fetching all users");
-});
+router.get("/", userController.getAllUsers);
 
-router.post("/", (req, res) => {
-  res.send("Adding a new user");
-});
+router.post("/", userController.addingNewUser);
 
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  res.send(`Fetching user with ID: ${id}`);
-});
+router.get("/:id", userController.getUserById);
 
 module.exports = router;
