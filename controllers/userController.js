326f8c3f9c181379a5e1ca3aaad4dc6c985309d9
mdshayequeahmad <shayeque01@gmail.com>
2026-03-25 20:01:57 +0530

    Understanding Centralised Error Handling

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,8 +6,16 @@ const addingNewUser = (req, res) => {
   res.send("Adding a new user");
 }
 
-const getUserById = (req, res) => {
+const getUserById = (req, res, next) => {
+
   const id = req.params.id;
+
+  if(!id){
+      const error = new Error("User ID is required");
+      error.statusCode = 400;
+      return next(error);
+  }
+
   res.send(`Fetching user with ID: ${id}`);
 }
 
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 const cartRoutes = require("./routes/cartRoutes");
 
+const notFound = require("./utils/notFound");
+const errorHandler = require("./utils/errorHandler");
+
 const app = express();
 
 app.use(express.static('public'));
@@ -11,6 +14,14 @@ app.use(express.json());
 app.use("/users", userRoutes);
 app.use("/products", productRoutes);
 app.use("/cart", cartRoutes);
+
+
+// 404 middleware
+app.use(notFound);
+
+// centralized error handler
+app.use(errorHandler);
+
 
 const PORT = 5000;
 
